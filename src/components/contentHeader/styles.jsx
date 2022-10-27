4650e3c2e480016521f690692ad13b05c418f06c
Th-Axel96 <axelmoussart77@hotmail.com>
2022-10-27 10:35:26 +0200

    create index.jsx & styles

---
+++ src/components/contentHeader/styles.jsx
@@ -0,0 +1,46 @@
+// content header desktop
+
+export const styles = {
+  // grid
+  grid: {
+    gridCol6: "grid grid-cols-6",
+    colStart2:"col-start-2"
+  },
+  //   flex
+  flex: {
+    justifyCenter: "flex justify-center",
+    itemsEnd: "flex items-end",
+    itemsCenter: "flex items-center",
+  },
+
+  fix: "fixed",
+
+  //    aside
+  aside: {
+    divBitmoji: "pt-4 flex justify-center ",
+    asideBg: "bg-gray-900 fixed w-80 h-screen",
+    h2Name:
+      "text-white text-3xl  font-open font-bold  flex justify-center pt-8",
+    navAside: "pl-8 text-xl text-gray-400",
+    navIcon: "text-3xl mr-2",
+    navLi: "py-8",
+    HoverBlue: "hover:text-blue-400",
+    // social icon
+    socialIconBg: "w-12 h-12 my-10 bg-gray-600 rounded-full hover:bg-blue-400",
+    socialIcon: "text-3xl text-white",
+    copyrightIco: "text-lg text-white",
+    copyrightDiv: "h-60 text-white",
+  },
+  // text
+  text: {
+    fontBold: "font-bold",
+    textWhite: "text-white",
+  },
+
+  // padding // margin
+  mx4: "mx-4",
+
+  // background
+bg:""
+
+};
