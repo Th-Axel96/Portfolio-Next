5ea1068e3a4231169de0fce1a17c565ad5126152
Th-Axel96 <axelmoussart77@hotmail.com>
2022-10-27 10:51:34 +0200

    try fix netlify

--- pages/_document.jsx
+++ pages/_document.jsx
@@ -1,4 +1,5 @@
 import { Html, Head, Main, NextScript } from "next/document";
+import Script from "next/script";
 
 export default function Document() {
   return (
@@ -24,7 +25,7 @@ export default function Document() {
           rel="stylesheet"
           href="https://cdn.jsdelivr.net/npm/tw-elements/dist/css/index.min.css"
         />
-        <script src="https://cdn.jsdelivr.net/npm/tw-elements/dist/js/index.min.js"></script>
+        <Script src="https://cdn.jsdelivr.net/npm/tw-elements/dist/js/index.min.js"></Script>
       </Head>
       <body>
         <Main />
