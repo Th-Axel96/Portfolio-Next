6664a1eb38e3b19312a45b17e5560b6c3a4bab48
Th-Axel96 <axelmoussart77@hotmail.com>
2022-10-27 10:25:19 +0200

    update entry point, add asideHeader & contentHeader

--- pages/home/index.jsx
+++ pages/home/index.jsx
@@ -1,13 +1,11 @@
-
-
-
-// header
-
+import { AsideHeader } from "@src/components/asideHeaderDesktop";
+import { ContentHeader } from "@src/components/contentHeader";
 
 const HomePage = () => {
   return (
     <>
-   
+   <AsideHeader />
+   <ContentHeader />
      
     </>
   );
