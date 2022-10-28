f44e04fcde0b7381194885467d6db54158978f62
Th-Axel96 <axelmoussart77@hotmail.com>
2022-10-28 15:02:51 +0200

    create aboutME index & styles

---
+++ src/components/aboutMe/index.jsx
@@ -0,0 +1,9 @@
+import { styles } from "./styles";
+
+export const AboutMe = () => {
+    return (
+        <div>
+            <h2>A propos</h2>
+        </div>
+    )
+}
---
+++ src/components/aboutMe/styles.jsx
@@ -0,0 +1,81 @@
+// content About me
+
+export const styles = {
+  // grid
+  grid: {
+    gridCol6: "grid grid-cols-6",
+    colStart2: "col-start-2 col-span-5",
+  },
+  //   flex
+  flex: {
+    justifyCenter: "flex justify-center",
+    itemsEnd: "flex items-end",
+    itemsCenter: "flex items-center",
+    justifyNameTyping: "h-5/6 flex flex-col justify-center items-center",
+  },
+
+  // background
+
+  bgGLass:
+    "w-5/12 h-40 bg-black/40 flex flex-col justify-center items-center rounded-full",
+
+  // height
+  hFull: "h-full",
+
+  fix: "fixed",
+
+  //    aside
+  aside: {
+    divBitmoji: "pt-4 flex justify-center ",
+    asideBg: "bg-gray-900 fixed w-80 h-screen",
+    h2Name:
+      "text-white text-3xl  font-open font-bold  flex justify-center pt-8",
+    navAside: "pl-8 text-xl text-gray-400",
+    navIcon: "text-3xl mr-2",
+    navLi: "py-8",
+    HoverBlue: "hover:text-blue-400",
+    // social icon
+    socialIconBg:
+      "w-12 h-12 my-10 bg-gray-600 rounded-full hover:bg-blue-400 hover:animate-pulse",
+    socialIcon: "text-3xl text-white",
+    copyrightIco: "text-lg text-white",
+    copyrightDiv: "h-20 text-white",
+    // burger menu mobile
+    burgerIco: "",
+    clearBurgerIco: " text-4xl text-white",
+  },
+
+  // text
+  text: {
+    fontBold: "font-bold",
+    textWhite: "text-white",
+  },
+
+  // padding // margin
+  mx4: "mx-4",
+
+  // contentHeader
+
+  contentHeader: {
+    bgHeader: "bg-person-coding bg-cover bg-no-repeat",
+    bgBlur: " h-screen w-full",
+    nameHeader:
+      "w-screen flex justify-center text-white text-2xl font-open font-bold",
+    spanTypingBorder:
+      "flex items-center  text-white font-open text-xl border-b-2 border-blue-400",
+    spanTyping:
+      "flex flex-col items-center w-screen text-white font-open text-xl pt-4",
+  },
+
+  // hidden block
+
+  lgBlock: "hidden lg:block",
+
+
+  // BODY
+body:{
+  
+}
+
+
+};
