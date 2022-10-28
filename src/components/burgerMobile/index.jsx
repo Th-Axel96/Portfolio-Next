7b1c644adbb02f3b52e6f2c2dfe230e19c6df0b6
Th-Axel96 <axelmoussart77@hotmail.com>
2022-10-28 15:04:45 +0200

    add burgerMobile , menu burger it's ok

---
+++ src/components/burgerMobile/index.jsx
@@ -0,0 +1,153 @@
+import { styles } from "./styles";
+
+// icon burger
+import MenuIcon from "@mui/icons-material/Menu";
+import ClearIcon from "@mui/icons-material/Clear";
+
+import Image from "next/image";
+import bitmoji from "../../assets/img/bitmoji.png";
+
+// icon NAV
+import HomeIcon from "@mui/icons-material/Home";
+import PersonIcon from "@mui/icons-material/Person";
+import DataObjectIcon from "@mui/icons-material/DataObject";
+import TopicIcon from "@mui/icons-material/Topic";
+import FileOpenIcon from "@mui/icons-material/FileOpen";
+
+// icon Social
+import LinkedInIcon from "@mui/icons-material/LinkedIn";
+import GitHubIcon from "@mui/icons-material/GitHub";
+import EmailIcon from "@mui/icons-material/Email";
+
+// icon copyright
+import CopyrightIcon from "@mui/icons-material/Copyright";
+
+export const BurgerMenu = () => {
+  return (
+    // btn for burger
+    <div className={styles.paddingIcoMenu}>
+      <button
+        type="button"
+        data-bs-toggle="offcanvas"
+        data-bs-target="#offcanvasExample"
+        aria-controls="offcanvasExample"
+      >
+        {/* ico menu burger */}
+        <MenuIcon className={styles.aside.burgerIco} />
+      </button>
+
+      {/* div for OFF CANVA */}
+      <div
+        class="offcanvas offcanvas-start  fixed bottom-0  bg-gray-600 invisible bg-clip-padding shadow-sm outline-none transition duration-1000 ease-in-out text-gray-700 top-0 left-0 border-none w-80"
+        tabindex="-1"
+        id="offcanvasExample"
+      >
+        {/* content aside fixed */}
+        <div className={styles.aside.asideBg}>
+          {/*div for  clear ICON */}
+          <div className="flex justify-end">
+            {/* bg icon rounded */}
+            <button
+            
+              className={`${styles.aside.socialIconBg} ${styles.marginIcoCLear}`}
+              type="button"
+              data-bs-toggle="offcanvas"
+              data-bs-target="#offcanvasExample"
+              aria-controls="offcanvasExample"
+            >
+              <ClearIcon className={`${styles.aside.clearBurgerIco}`} />
+            </button>
+          </div>
+
+          {/* bitmoji */}
+          <div className={styles.aside.divBitmoji}>
+            <Image src={bitmoji} width="125px" height="125px" />
+          </div>
+
+          {/* flex col content aside */}
+          <h2 className={styles.aside.h2Name}>Moussart Axel</h2>
+          {/* div for social icon */}
+          <div className={`${styles.flex.justifyCenter} ${styles.mb8}`}>
+            <div
+              className={`${styles.aside.socialIconBg} ${styles.flex.justifyCenter} ${styles.flex.itemsCenter} `}
+            >
+              <GitHubIcon className={styles.aside.socialIcon} />
+            </div>
+            <div
+              className={`${styles.aside.socialIconBg} ${styles.flex.justifyCenter} ${styles.flex.itemsCenter} ${styles.mx4}`}
+            >
+              <LinkedInIcon className={`${styles.aside.socialIcon} `} />
+            </div>
+            <div
+              className={`${styles.aside.socialIconBg} ${styles.flex.justifyCenter} ${styles.flex.itemsCenter}`}
+            >
+              <EmailIcon className={styles.aside.socialIcon} />
+            </div>
+          </div>
+          {/* aside nav */}
+          <nav className={styles.aside.navAside}>
+            <ul>
+              {/* link home */}
+              <li className={styles.aside.navLi}>
+                <a
+                  className={`${styles.aside.HoverBlue} ${styles.flex.itemsEnd}`}
+                  href="#"
+                >
+                  <HomeIcon className={styles.aside.navIcon} />
+                  Accueil
+                </a>
+              </li>
+              {/* link about me */}
+              <li>
+                <a
+                  className={`${styles.aside.HoverBlue} ${styles.flex.itemsEnd}`}
+                  href="#"
+                >
+                  <PersonIcon className={styles.aside.navIcon} />A propos de moi
+                </a>
+              </li>
+              {/* link skils */}
+              <li className={styles.aside.navLi}>
+                <a
+                  className={`${styles.aside.HoverBlue} ${styles.flex.itemsEnd}`}
+                  href="#"
+                >
+                  <DataObjectIcon className={styles.aside.navIcon} />
+                  Mes compétences
+                </a>
+              </li>
+              <li>
+                <a
+                  className={`${styles.aside.HoverBlue} ${styles.flex.itemsEnd}`}
+                  href="#"
+                >
+                  <TopicIcon className={styles.aside.navIcon} />
+                  Mes projets
+                </a>
+              </li>
+              <li className={styles.aside.navLi}>
+                <a
+                  className={`${styles.aside.HoverBlue} ${styles.flex.itemsEnd}`}
+                  href="#"
+                >
+                  <FileOpenIcon className={styles.aside.navIcon} />
+                  CV
+                </a>
+              </li>
+            </ul>
+          </nav>
+          {/* copyright */}
+          <div
+            className={`${styles.flex.itemsEnd} ${styles.aside.copyrightDiv} ${styles.flex.justifyCenter}`}
+          >
+            <p>
+              <CopyrightIcon className={`${styles.aside.copyrightIco} `} />
+              Copyright &nbsp;
+              <span className={styles.text.fontBold}>Moussart Axel</span>
+            </p>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
---
+++ src/components/burgerMobile/styles.jsx
@@ -0,0 +1,71 @@
+// Burger menu mobile
+
+export const styles = {
+  // grid
+  grid: {
+    gridCol6: "grid grid-cols-6",
+    colStart2: "col-start-2 col-span-5",
+  },
+  //   flex
+  flex: {
+    justifyCenter: "flex justify-center",
+    itemsEnd: "flex items-end",
+    itemsCenter: "flex items-center",
+    justifyColItemsCenter: "flex flex-col justify-center items-center",
+  },
+
+  // background
+
+  bgGLass:
+    "w-5/12 h-40 bg-black/40 flex flex-col justify-center items-center rounded-full",
+
+  // height
+  hFull: "h-full",
+
+  //    aside
+  aside: {
+    divBitmoji: " flex justify-center ",
+    asideBg: "bg-gray-900 w-80 h-screen",
+    h2Name:
+      "text-white text-3xl  font-open font-bold  flex justify-center py-8",
+    navAside: "pl-8 text-xl text-gray-400",
+    navIcon: "text-3xl mr-2",
+    navLi: "py-8",
+    HoverBlue: "hover:text-blue-400",
+    // social icon
+    socialIconBg:
+      "w-12 h-12  bg-gray-600 rounded-full hover:bg-blue-400 hover:animate-pulse",
+    socialIcon: "text-3xl text-white",
+    copyrightIco: "text-lg text-white",
+    copyrightDiv: "h-20 text-white",
+    // burger menu mobile
+    burgerIco: "text-5xl text-white",
+    clearBurgerIco: " text-4xl text-white",
+  },
+
+  // text
+  text: {
+    fontBold: "font-bold",
+    textWhite: "text-white",
+  },
+
+  // padding // margin
+  mx4: "mx-4",
+  mb8: "mb-8",
+  marginIcoCLear: "mt-2 mr-2",
+  paddingIcoMenu:"pl-4 pt-4",
+
+  // contentHeader
+
+  contentHeader: {
+    bgHeader: "bg-person-coding bg-cover bg-no-repeat",
+    bgBlur: " h-screen w-full",
+    nameHeader: "text-white text-5xl font-open font-bold",
+    spanTyping:
+      "flex text-white font-open text-3xl border-b-2 border-blue-400 pt-4",
+  },
+
+  // hidden block
+
+  lgBlock: "hidden lg:block",
+};
