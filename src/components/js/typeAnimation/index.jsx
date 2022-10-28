f74795b9dd20f74c5249fc98492f97b4cef51f20
Th-Axel96 <axelmoussart77@hotmail.com>
2022-10-28 15:08:01 +0200

    add components with JS

---
+++ src/components/js/typeAnimation/index.jsx
@@ -0,0 +1,21 @@
+import { TypeAnimation } from "react-type-animation";
+
+export const Typing = () => {
+  return (
+    <TypeAnimation
+      sequence={[
+        "Front-End", // Types 'One'
+        2000, // Waits 1s
+        "Back-End", // Deletes 'One' and types 'Two'
+        2000, // Waits 2s
+        "Full-Stack", // Types 'Three' without deleting 'Two'
+        2000,
+      ]}
+      speed={5}
+      wrapper="div"
+      cursor={false}
+      repeat={Infinity}
+      
+    />
+  );
+};
