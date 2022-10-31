4e1de6b76f55cb73349d8edc35811aad51c55518
Th-Axel96 <axelmoussart77@hotmail.com>
2022-10-31 14:18:59 +0100

    add content to entry point

--- pages/home/index.jsx
+++ pages/home/index.jsx
@@ -1,11 +1,13 @@
 import { HeaderGlobal } from "@src/components/headerGlobal";
 import { AboutMe } from "@src/components/aboutMe";
+import { SectionSkills } from "@src/components/sectionSkills";
 
 const HomePage = () => {
   return (
     <>
       <HeaderGlobal />
-     <AboutMe />
+      <AboutMe />
+      <SectionSkills />
     </>
   );
 };
