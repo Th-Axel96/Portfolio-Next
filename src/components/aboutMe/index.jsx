de1068dbfe556018ba1cf1a70dc54575e9036f0f
Th-Axel96 <axelmoussart77@hotmail.com>
2022-10-31 14:28:22 +0100

    remplace ' on &apos; for push on netlify

--- src/components/aboutMe/index.jsx
+++ src/components/aboutMe/index.jsx
@@ -10,17 +10,17 @@ export const AboutMe = () => {
       {/* div for broder title */}
       <div className={styles.body.borderTitle}></div>
       {/* img illustration */}
-     
-        <Image src={ImgCoding} />
-      
+
+      <Image src={ImgCoding} />
+
       {/* paragraph content about me */}
       <p className={`${styles.body.paragraph} ${styles.pt6}`}>
-        Passionné par le monde de l'informatique, j'ai commencé à jouer à des
-        jeux en ligne (MMORPG, RTS, Hack 'n' slash) et parcourir le web depuis
-        mon plus jeune âge.
+        Passionné par le monde de l&apos;informatique, j&apos;ai commencé à
+        jouer à des jeux en ligne (MMORPG, RTS, Hack &apos;n&apos; slash) et
+        parcourir le web depuis mon plus jeune âge.
       </p>
       <p className={`${styles.body.paragraph} ${styles.pt6}`}>
-        Peu de temps après, j'ai commencé à me tourner vers le Hardware, en
+        Peu de temps après, j&apos;ai commencé à me tourner vers le Hardware, en
         démontant, changeant les pièces des PC et assemblé des configurations.
       </p>
       <p className={`${styles.body.paragraph} ${styles.pt6}`}>
@@ -28,12 +28,12 @@ export const AboutMe = () => {
         donc décidé de me tourner vers le développement.
       </p>
       <p className={`${styles.body.paragraph} ${styles.pt6}`}>
-        Partant d'aucune connaissance dans ce domaine, j'effectue une formation
-        de développeur web et web mobile.
+        Partant d&apos;aucune connaissance dans le domaine du developpement,
+        j&apos;effectue une formation de développeur web et web mobile.
       </p>
       <p className={`${styles.body.paragraph} ${styles.pt6}`}>
-        Je suis donc à la recherche d'un stage pour validé mon titre
-        professionnelle et d'autres opportunités qui me seront offerte.
+        Je suis donc à la recherche d&apos;un stage pour validé mon titre
+        professionnelle et d&apos;autres opportunités qui me seront offerte.
       </p>
     </div>
   );
