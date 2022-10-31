db19bdf0793ddeabe8fed916bc5f0d8a369d03f3
Th-Axel96 <axelmoussart77@hotmail.com>
2022-10-31 12:28:34 +0100

    creat new function, Rotate img

---
+++ src/components/js/RotateImgHeader/index.jsx
@@ -0,0 +1,35 @@
+import React, { useState, useEffect, useRef } from "react";
+import { gsap } from "gsap";
+
+// img header
+import Image from "next/image";
+import DevImgHeader from "../../../assets/img/Developer-img.svg"
+
+/* test rotate its ok */
+export const RotateAnimation = () => {
+  const [toggle, setToggle] = useState(false);
+  const changeState = () => {
+    setToggle(!toggle);
+  };
+  const cardRef = useRef(null);
+
+  useEffect(() => {
+    toggle
+      ? gsap.to(cardRef.current, {
+          
+          rotate: 30,
+          duration: 1.2,
+        })
+      : gsap.to(cardRef.current, {
+          
+          rotate: 0,
+          duration: 1.2,
+        });
+  }, [toggle]);
+
+  return (
+    <div  onMouseOver={changeState} ref={cardRef}>
+      <Image src={DevImgHeader} width="230px" height="230px" />
+    </div>
+  );
+};
