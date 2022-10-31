3128c153dae230ca6ac3d7dfd28f010539e1a39a
Th-Axel96 <axelmoussart77@hotmail.com>
2022-10-31 14:22:30 +0100

    add onclick for function IMG

--- src/components/js/RotateImgHeader/index.jsx
+++ src/components/js/RotateImgHeader/index.jsx
@@ -33,3 +33,38 @@ export const RotateAnimation = () => {
     </div>
   );
 };
+
+
+
+// import React, { useState, useEffect, useRef } from "react";
+// import { gsap } from "gsap";
+
+// /* test card */
+// export const CardTest = () => {
+//   const [toggle, setToggle] = useState(false);
+//   const changeState = () => {
+//     setToggle(!toggle);
+//   };
+//   const cardRef = useRef(null);
+
+//   useEffect(() => {
+//     toggle ? 
+//     gsap.to(cardRef.current, {
+//       scale: 1.5,
+//       rotate: 360,
+//       duration: 1.2,
+
+//     })
+//     : gsap.to(cardRef.current, {
+//       scale:1,
+//       rotate:0,
+//       duration:1.2,
+//     })
+//   }, [toggle]);
+
+//   return (
+//     <div className={styles.card} onClick={changeState} ref={cardRef}>
+//       Amsterdam
+//     </div>
+//   );
+// };
