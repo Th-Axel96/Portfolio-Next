b50575932485cd572934d09acede9076e00317c8
Th-Axel96 <axelmoussart77@hotmail.com>
2022-10-31 14:23:32 +0100

    creat section Skills and add content, icon with my skilss

---
+++ src/components/sectionSkills/index.jsx
@@ -0,0 +1,71 @@
+import { styles } from "./styles";
+import Image from "next/image";
+import IconHtml from "../../assets/img/icon-html.svg";
+import IconCss from "../../assets/img/icon-css.svg";
+import IconJs from "../../assets/img/icon-js.svg";
+import IconNode from "../../assets/img/icon-node.svg";
+import IconReact from "../../assets/img/icon-react.svg";
+import IconPhp from "../../assets/img/icon-php.svg"
+
+export const SectionSkills = () => {
+  return (
+    // contenair
+    <div className={styles.mx4}>
+      {/* title */}
+      <h2 className={`${styles.body.title} ${styles.pt6}`}>Mes compétences</h2>
+      {/* div for border title */}
+      <div className={styles.body.borderTitle}></div>
+      {/* div for grid  */}
+      <div className={`${styles.grid.gridCol2} `}>
+        {/* div for justify-content */}
+        <div className={styles.flex.justifyCenter}>
+          {/* box icon HTML */}
+          <div className={styles.boxIconSkills}>
+            <Image src={IconHtml} />
+            Html
+          </div>
+        </div>
+        {/* div for justify-content*/}
+        <div className={styles.flex.justifyCenter}>
+          {/* box icon CSS */}
+          <div className={styles.boxIconSkills}>
+            <Image src={IconCss} />
+            Css
+          </div>
+        </div>
+        {/* div for justify-content*/}
+        <div className={styles.flex.justifyCenter}>
+          {/* box icon JS */}
+          <div className={styles.boxIconSkills}>
+            <Image src={IconJs} />
+            Javascript
+          </div>
+        </div>
+        {/* div for justify-content*/}
+        <div className={styles.flex.justifyCenter}>
+          {/* box icon Node */}
+          <div className={styles.boxIconSkills}>
+            <Image src={IconNode} />
+            Node.js
+          </div>
+        </div>
+        {/* div for justify-content*/}
+        <div className={styles.flex.justifyCenter}>
+          {/* box icon React */}
+          <div className={styles.boxIconSkills}>
+            <Image src={IconReact} />
+            React
+          </div>
+        </div>
+        {/* div for justify-content*/}
+        <div className={styles.flex.justifyCenter}>
+          {/* box icon PHP */}
+          <div className={styles.boxIconSkills}>
+            <Image src={IconPhp} />
+            Php
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
---
+++ src/components/sectionSkills/styles.jsx
@@ -0,0 +1,47 @@
+// content sectionSkills
+
+export const styles = {
+  // grid
+  grid: {
+    gridCol6: "grid grid-cols-6",
+    gridCol2: "grid grid-cols-2",
+    colStart2: "col-start-2 col-span-5",
+  },
+  //   flex
+  flex: {
+    justifyCenter: "flex justify-center",
+    itemsEnd: "flex items-end",
+    itemsCenter: "flex items-center",
+    justifyNameTyping: "h-5/6 flex flex-col justify-center items-center",
+  },
+
+  // background
+
+  bgGLass:
+    "w-5/12 h-40 bg-black/40 flex flex-col justify-center items-center rounded-full",
+
+  // height
+  hFull: "h-full",
+
+  fix: "fixed",
+
+  // padding // margin
+  mx4: "mx-4",
+  pt6: "pt-6",
+
+  // contentHeader
+
+  // hidden block
+
+  lgBlock: "hidden lg:block",
+
+  // BODY
+  body: {
+    title: "text-2xl text-indigo-800 uppercase font-open font-bold pt-8",
+    borderTitle: "border-b-4 border-blue-400 w-20 mt-2 mb-6",
+    paragraph: "font-open text-sm font-semibold",
+  },
+  // box ICON skilss
+  boxIconSkills:
+    "flex flex-col text-white font-open text-lg font-bold uppercase justify-center items-center w-40 h-32 bg-black rounded-xl border-t-4 border-blue-400 p-4 mb-4 hover:scale-110 transition duration-1000 ease-in-out hover:bg-gray-300 hover:text-black",
+};
