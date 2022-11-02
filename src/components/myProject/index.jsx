cd8d6968ace280b384205d17dc2d7a04b73c594b
Th-Axel96 <axelmoussart77@hotmail.com>
2022-11-02 13:33:26 +0100

    create index & styles and add content for section Myprojects

---
+++ src/components/myProject/index.jsx
@@ -0,0 +1,134 @@
+import { styles } from "./styles";
+import Image from "next/image";
+import TpLandingPage from "../../assets/img/tp-landing-page.png";
+import TpMazeBank from "../../assets/img/tp-mazebank.png";
+import CvVanilla from "../../assets/img/cv-axel-html.png";
+import TpWanaAgency from "../../assets/img/wana-agency.png";
+
+export const SectionProject = () => {
+  return (
+    // grid col 6
+    <div className={styles.grid.gridCol6}>
+      {/* grid start 2 */}
+      <div className={styles.grid.colStart2}>
+        {/* div contenair */}
+        <div className={styles.containerDiv}>
+          {/* title */}
+          <h2 className={styles.body.title}>Mes projets</h2>
+          {/* div for border */}
+          <div className={styles.body.borderTitle}></div>
+
+          <p className={styles.body.paragraph}>
+            Voici la listes des projets que j&apos;ai réalisé au cours de ma
+            formation.
+          </p>
+
+          {/* div for lg grid  */}
+          <div className={styles.card.CardGrid}>
+            {/* cards Landing Page */}
+            <div className={styles.flex.justifyCenter}>
+              <div className={styles.card.contentCard}>
+                <div data-mdb-ripple="true" data-mdb-ripple-color="light">
+                  <Image src={TpLandingPage} className={styles.card.CardImg} />
+                </div>
+
+                <div className={styles.p6}>
+                  <h5 className={styles.card.titleProject}>TP Landing Page</h5>
+                  <p className={styles.card.paragraph}>
+                    Projet réalisé avec Bootstrap, reproduire une Landing Page.
+                  </p>
+                  <div className={styles.flex.justifyCenter}>
+                    <button type="button" className={styles.card.btnLink}>
+                      <a
+                        href="https://tp-landingpage.netlify.app/"
+                        target="_blank"
+                      >
+                        Voir le projet
+                      </a>
+                    </button>
+                  </div>
+                </div>
+              </div>
+            </div>
+            {/* cards Maze Bank */}
+            <div className={styles.flex.justifyCenter}>
+              <div className={styles.card.contentCard}>
+                <div data-mdb-ripple="true" data-mdb-ripple-color="light">
+                  <Image src={TpMazeBank} className={styles.card.CardImg} />
+                </div>
+
+                <div className={styles.p6}>
+                  <h5 className={styles.card.titleProject}>TP Landing Page</h5>
+                  <p className={styles.card.paragraph}>
+                    Réalisé une application bancaire avec de l&apos;algorythme
+                    Javascript.
+                  </p>
+                  <div className={styles.flex.justifyCenter}>
+                    <button type="button" className={styles.card.btnLink}>
+                      <a
+                        href="https://axel-mazebank.netlify.app/"
+                        target="_blank"
+                      >
+                        Voir le projet
+                      </a>
+                    </button>
+                  </div>
+                </div>
+              </div>
+            </div>
+            {/* cards Wana Agency */}
+            <div className={styles.flex.justifyCenter}>
+              <div className={styles.card.contentCard}>
+                <div data-mdb-ripple="true" data-mdb-ripple-color="light">
+                  <Image src={TpWanaAgency} className={styles.card.CardImg} />
+                </div>
+
+                <div className={styles.p6}>
+                  <h5 className={styles.card.titleProject}>TP Agence Web</h5>
+                  <p className={styles.card.paragraph}>
+                    Projet de groupe, réalisé une Agence Web.
+                  </p>
+                  <div className={styles.flex.justifyCenter}>
+                    <button type="button" className={styles.card.btnLink}>
+                      <a
+                        href="https://wana-agency.netlify.app/"
+                        target="_blank"
+                      >
+                        Voir le projet
+                      </a>
+                    </button>
+                  </div>
+                </div>
+              </div>
+            </div>
+            {/* cards CV HTML*/}
+            <div className={styles.flex.justifyCenter}>
+              <div className={styles.card.contentCard}>
+                <div data-mdb-ripple="true" data-mdb-ripple-color="light">
+                  <Image src={CvVanilla} className={styles.card.CardImg} />
+                </div>
+
+                <div className={styles.p6}>
+                  <h5 className={styles.card.titleProject}>Cv Html</h5>
+                  <p className={styles.card.paragraph}>
+                    Réalisé un CV en HTML/CSS vanilla.
+                  </p>
+                  <div className={styles.flex.justifyCenter}>
+                    <button type="button" className={styles.card.btnLink}>
+                      <a
+                        href="https://moussart-cv.netlify.app/"
+                        target="_blank"
+                      >
+                        Voir le projet
+                      </a>
+                    </button>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
---
+++ src/components/myProject/styles.jsx
@@ -0,0 +1,64 @@
+// content My Project
+
+export const styles = {
+  // grid
+  grid: {
+    gridCol6: "xl:grid grid-cols-6",
+    gridCol2: "md:grid md:grid-cols-2",
+    colStart2: "col-start-2 col-span-5",
+    colStart3: "col-start-3 col-span-5",
+    lgGridCol3: "lg:grid lg:grid-cols-3",
+  },
+  //   flex
+  flex: {
+    justifyCenter: "flex justify-center",
+    itemsEnd: "flex items-end",
+    itemsCenter: "flex items-center",
+    justifyNameTyping: "h-5/6 flex flex-col justify-center items-center",
+    colCenter: "flex flex-col justify-center xl:mr-32",
+  },
+
+  // background
+
+  bgGLass:
+    "w-5/12 h-40 bg-black/40 flex flex-col justify-center items-center rounded-full",
+
+  // height
+  hFull: "h-full",
+
+  fix: "fixed",
+
+  // padding // margin
+  mx4: "mx-4",
+  pt6: "pt-6",
+  p6: "p-6",
+
+  // container
+  containerDiv: "mx-4 xl:ml-32",
+
+  // contentHeader
+
+  // hidden block
+
+  lgBlock: "hidden lg:block",
+
+  // BODY
+  body: {
+    title: "text-2xl text-indigo-800 uppercase font-open font-bold pt-8",
+    borderTitle: "border-b-4 border-blue-400 w-20 mt-2 mb-6",
+    paragraph: "font-open text-sm lg:text-xl font-semibold",
+  },
+
+  card: {
+    contentCard:
+      " rounded-lg shadow-lg bg-gray-300 max-w-xs mt-8 border-x border-gray-400",
+    titleProject:
+      "uppercase flex justify-center text-xl font-open font-bold mb-4",
+    paragraph:
+      " text-center font-open text-sm lg:text-lg font-semibold border-y-2 border-gray-400 py-4",
+    CardImg: "rounded-t-lg",
+    btnLink:
+      " inline-block px-6 py-2.5 bg-gray-600 text-white font-medium text-sm lg:text-lg leading-tight uppercase rounded shadow-md hover:bg-blue-400 hover:shadow-lg focus:bg-blue-400 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-blue-400 active:shadow-lg transition duration-1000 ease-in-out mt-6",
+    CardGrid: "lg:grid lg:grid-cols-3 md:grid md:grid-cols-2",
+  },
+};
