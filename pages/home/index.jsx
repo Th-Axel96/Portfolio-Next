db98268940c84c2e5b37d180bc0acc222914bf46
Th-Axel96 <axelmoussart77@hotmail.com>
2022-11-02 13:25:16 +0100

    update, add global background

--- pages/home/index.jsx
+++ pages/home/index.jsx
@@ -1,17 +1,17 @@
 import { HeaderGlobal } from "@src/components/headerGlobal";
 import { AboutMe } from "@src/components/aboutMe";
 import { SectionSkills } from "@src/components/sectionSkills";
+import { SectionProject } from "@src/components/myProject";
+import { SectionContact } from "@src/components/contact";
 
 const HomePage = () => {
   return (
     <>
       <HeaderGlobal />
-      
-
-
       <AboutMe />
       <SectionSkills />
-      
+      <SectionProject />
+      <SectionContact />
     </>
   );
 };
