3e04f1ccb037d4ba0125616f17a5d2428d5ec846
Th-Axel96 <axelmoussart77@hotmail.com>
2022-11-05 17:27:43 +0100

    add AOS animate on content

--- src/components/contentHeaderMobile/index.jsx
+++ src/components/contentHeaderMobile/index.jsx
@@ -1,48 +1,51 @@
-import { styles } from "./styles"
+import { styles } from "./styles";
 import Image from "next/image";
 // import burger menu
 import { BurgerMenu } from "../burgerMobile";
 import { Typing } from "../js/typeAnimation";
 
 // img
-import DevImgHeader from "../../assets/img/Developer-img.svg"
+import DevImgHeader from "../../assets/img/Developer-img.svg";
 import { RotateAnimation } from "../js/RotateImgHeader";
-
-
 
 export const ContentHeaderMobile = () => {
   return (
     <>
-      <div >
+      <div>
         {/*  */}
-        <BurgerMenu  />
-        <div
-          className={`${styles.flex.justifyNameTyping} `}
-        >
-       
-          
-            <h1 className={`${styles.contentHeader.nameHeader}`}>
-              Portfolio Moussart Axel
-            </h1>
-            <h2 className={styles.contentHeader.spanTyping}>
-              Je suis Développeur
-              <span className={styles.contentHeader.spanTypingBorder}>
-                Web&nbsp;
-                <Typing />
-              </span>
-            </h2>
-{/* rotate IMG */}
-<div className={styles.py20}>
-
-<RotateAnimation />
-</div>
-
-          
+        <BurgerMenu />
+        <div className={`${styles.flex.justifyNameTyping} `}>
+          <h1
+            data-aos="fade-up-right"
+            data-aos-delay="50"
+            data-aos-duration="2000"
+            className={`${styles.contentHeader.nameHeader}`}
+          >
+            Portfolio Moussart Axel
+          </h1>
+          <h2
+            data-aos="fade-up-right"
+            data-aos-delay="50"
+            data-aos-duration="2000"
+            className={styles.contentHeader.spanTyping}
+          >
+            Je suis Développeur
+            <span className={styles.contentHeader.spanTypingBorder}>
+              Web&nbsp;
+              <Typing />
+            </span>
+          </h2>
+          {/* rotate IMG */}
+          <div
+            data-aos="flip-right"
+            data-aos-delay="50"
+            data-aos-duration="2000"
+            className={styles.py20}
+          >
+            <RotateAnimation />
+          </div>
         </div>
       </div>
     </>
   );
 };
-
-
-
