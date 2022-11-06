1f271388111987165230cb70e978c7668d6e44ac
Th-Axel96 <axelmoussart77@hotmail.com>
2022-11-06 15:22:08 +0100

    add id for ancre

--- src/components/myProject/index.jsx
+++ src/components/myProject/index.jsx
@@ -12,7 +12,7 @@ export const SectionProject = () => {
       {/* grid start 2 */}
       <div className={styles.grid.colStart2}>
         {/* div contenair */}
-        <div className={styles.containerDiv}>
+        <div id="myProject" className={styles.containerDiv}>
           {/* div for AOS title & border */}
           <div
             data-aos="fade-right"
@@ -31,7 +31,7 @@ export const SectionProject = () => {
             data-aos-duration="2000"
             className={styles.body.paragraph}
           >
-            Voici la listes des projets que j&apos;ai réalisé au cours de ma
+            Voici la liste des projets que j&apos;ai réalisé au cours de ma
             formation.
           </p>
 
@@ -52,7 +52,7 @@ export const SectionProject = () => {
                 <div className={styles.p6}>
                   <h5 className={styles.card.titleProject}>TP Landing Page</h5>
                   <p className={styles.card.paragraph}>
-                    Projet réalisé avec Bootstrap, reproduire une Landing Page.
+                    Projet réaliser avec Bootstrap, reproduire une Landing Page.
                   </p>
                   <div className={styles.flex.justifyCenter}>
                     <button type="button" className={styles.card.btnLink}>
@@ -79,7 +79,7 @@ export const SectionProject = () => {
                 <div className={styles.p6}>
                   <h5 className={styles.card.titleProject}>TP Landing Page</h5>
                   <p className={styles.card.paragraph}>
-                    Réalisé une application bancaire avec de l&apos;algorythme
+                    Concevoir une application bancaire avec de l&apos;algorythme
                     Javascript.
                   </p>
                   <div className={styles.flex.justifyCenter}>
@@ -107,7 +107,7 @@ export const SectionProject = () => {
                 <div className={styles.p6}>
                   <h5 className={styles.card.titleProject}>TP Agence Web</h5>
                   <p className={styles.card.paragraph}>
-                    Projet de groupe, réalisé une Agence Web.
+                    Projet de groupe, réaliser une Agence Web.
                   </p>
                   <div className={styles.flex.justifyCenter}>
                     <button type="button" className={styles.card.btnLink}>
@@ -134,7 +134,7 @@ export const SectionProject = () => {
                 <div className={styles.p6}>
                   <h5 className={styles.card.titleProject}>Cv Html</h5>
                   <p className={styles.card.paragraph}>
-                    Réalisé un CV en HTML/CSS vanilla.
+                    Rédiger un CV en HTML/CSS vanilla.
                   </p>
                   <div className={styles.flex.justifyCenter}>
                     <button type="button" className={styles.card.btnLink}>
