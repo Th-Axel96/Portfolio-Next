f73182e2a37fdcd0845603ec6fa24c2b720c6a40
Th-Axel96 <axelmoussart77@hotmail.com>
2022-11-08 09:07:46 +0100

    add backgound img custom

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,9 @@ module.exports = {
   content: ["./pages/**/*.{js,ts,jsx,tsx}", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
     extend: {
+      backgroundImage: {
+        "person-coding": "url('/person-coding-blur.jpg')",
+      },
       colors: {
         gold: "#f8b700",
         darkBlue: "#0000D1",
