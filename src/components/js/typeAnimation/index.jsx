ecc127da5605303e09a88047a6128d2f3a1b3f74
Th-Axel96 <axelmoussart77@hotmail.com>
2022-11-16 16:45:41 +0100

    update content text typing animate

--- src/components/js/typeAnimation/index.jsx
+++ src/components/js/typeAnimation/index.jsx
@@ -8,10 +8,10 @@ export const Typing = () => {
         2000, // Waits 1s
         "Back-End", // Deletes 'One' and types 'Two'
         2000, // Waits 2s
-        "Full-Stack", // Types 'Three' without deleting 'Two'
+        "FullStack", // Types 'Three' without deleting 'Two'
         2000,
       ]}
-      speed={50}
+      speed={30}
       wrapper="div"
       cursor={false}
       repeat={Infinity}
