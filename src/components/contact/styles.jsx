472f2e2b0486a15121cc47eeaada6fd6d7985c88
Th-Axel96 <axelmoussart77@hotmail.com>
2022-11-16 16:37:58 +0100

    update styles css for box  border top

--- src/components/contact/styles.jsx
+++ src/components/contact/styles.jsx
@@ -70,7 +70,7 @@ export const styles = {
   contact: {
     
     containerContact:
-      "bg-black text-white py-4 w-60 lg:w-72 xl:w-80 border-b-4 border-blue-400 flex flex-col justify-center items-center rounded-xl hover:scale-110 transition duration-1000 ease-in-out hover:bg-gray-300 hover:text-black mb-8",
+      "bg-black text-white py-4 w-60 lg:w-72 xl:w-80 border-t-4 border-blue-400 flex flex-col justify-center items-center rounded-xl hover:scale-110 transition duration-1000 ease-in-out hover:bg-gray-300 hover:text-black mb-8",
     sizeIcon: "text-3xl mb-4",
   },
 };
